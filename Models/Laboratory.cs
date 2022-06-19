f848de4154d02093cfc56c4688c162629f7c8167
DaikiSakuragi <daikisakuragi@outlook.com>
2022-06-19 16:57:46 -0300

    Adicionando a classe LaboratoryRepository

--- Models/Laboratory.cs
+++ Models/Laboratory.cs
@@ -1,13 +1,13 @@
-using LabManager.Models;
+namespace LabManager.Models;
 
-class Lab
+class Laboratory
 {
     public int Id { get; set; }
     public int Number { get; set; }
     public string Name { get; set; }
     public string Block { get; set; }
 
-    public Lab(int id, int number, string name, string block)
+    public Laboratory(int id, int number, string name, string block)
     {
         Id = id;
         Number = number;
---
+++ Reposories/LaboratoryRepository.cs
@@ -0,0 +1,45 @@
+using LabManager.Database;
+using LabManager.Models;
+using Microsoft.Data.Sqlite;
+
+namespace LabManager.Repositories;
+
+class LaboratoryRepository
+{
+
+    private readonly DatabaseConfig _databaseConfig;
+
+    public LaboratoryRepository(DatabaseConfig databaseConfig)    
+    {
+        _databaseConfig = databaseConfig;
+    }
+
+    public List<Laboratory> GetAll()
+    {
+
+        var laboratorys = new List<Laboratory>();
+        var connection = new SqliteConnection(_databaseConfig.ConnectionString);
+        connection.Open();
+
+        var command = connection.CreateCommand();
+        command.CommandText = "SELECT * FROM Laboratorys;";    
+
+        var reader = command.ExecuteReader();
+
+        while (reader.Read())
+        {
+            var laboratory = ReaderToLaboratory(reader);
+            laboratorys.Add(laboratory);
+
+        }
+
+        connection.Close();
+
+        return laboratorys;
+    }
+
+     private Laboratory ReaderToLaboratory(SqliteDataReader reader)
+    {
+        return new Laboratory(reader.GetInt32(0), reader.GetInt32(1), reader.GetString(2),  reader.GetString(3));
+    }
+}
